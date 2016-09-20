69420314d925db597b8ae0ba49892184fdb589b3
741109004 <741109004@qq.com>
2016-09-20 12:02:33 +0800

    3.1

--- app/src/main/java/com/qf/project/teamproject/MyApp.java
+++ app/src/main/java/com/qf/project/teamproject/MyApp.java
@@ -6,5 +6,8 @@ import android.app.Application;
  * Created by Administrator on 2016/9/20.
  */
 public class MyApp extends Application{
-
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
 }
