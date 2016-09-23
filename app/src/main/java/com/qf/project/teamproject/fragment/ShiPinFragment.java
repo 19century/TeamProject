4ee290fe9240bd505dbcac92236aa0089ddb958f
741109004 <741109004@qq.com>
2016-09-23 09:56:55 +0800

    视频界面造假数据

--- app/src/main/java/com/qf/project/teamproject/fragment/ShiPinFragment.java
+++ app/src/main/java/com/qf/project/teamproject/fragment/ShiPinFragment.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -19,8 +18,7 @@ import com.handmark.pulltorefresh.library.PullToRefreshBase;
 import com.qf.project.teamproject.R;
 import com.qf.project.teamproject.adapter.ShiPinAdapter;
 import com.qf.project.teamproject.customview.PullToRefreshRecyclerView;
-import com.qf.project.teamproject.model.FakeData;
-import com.qf.project.teamproject.model.ShiPinData;
+
 import com.qf.project.teamproject.model.ShiPinDataNew;
 import com.qf.project.teamproject.ui.ShiPinPlayerActivity;
 
@@ -28,11 +26,11 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
-import javax.security.auth.login.LoginException;
 
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.onekeyshare.OnekeyShare;
 import cn.xiaochuankeji.netcrypto.NetCrypto;
 
 import okhttp3.Call;
@@ -247,9 +245,43 @@ public class ShiPinFragment extends BaseFragment implements Handler.Callback,Pul
                 startActivity(intent);
                 break;
             case 5://分享
-
+                showShare();
                 break;
         }
 
     }
+
+    private void showShare() {
+        ShareSDK.initSDK(getActivity());
+        OnekeyShare oks = new OnekeyShare();
+        //关闭sso授权
+        oks.disableSSOWhenAuthorize();
+
+// 分享时Notification的图标和文字  2.5.9以后的版本不调用此方法
+        //oks.setNotification(R.drawable.ic_launcher, getString(R.string.app_name));
+        // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
+        oks.setTitle(getString(R.string.share));
+        // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
+        oks.setTitleUrl("http://sharesdk.cn");
+        // text是分享文本，所有平台都需要这个字段
+        oks.setText("我是分享文本");
+        //分享网络图片，新浪微博分享网络图片需要通过审核后申请高级写入接口，否则请注释掉测试新浪微博
+        oks.setImageUrl("http://f1.sharesdk.cn/imgs/2014/02/26/owWpLZo_638x960.jpg");
+        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+        //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+        // url仅在微信（包括好友和朋友圈）中使用
+        oks.setUrl("http://sharesdk.cn");
+        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+        oks.setComment("我是测试评论文本");
+        // site是分享此内容的网站名称，仅在QQ空间使用
+        oks.setSite(getString(R.string.app_name));
+        // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+        oks.setSiteUrl("http://sharesdk.cn");
+
+// 启动分享GUI
+        oks.show(getActivity());
+    }
+
+
+
 }
